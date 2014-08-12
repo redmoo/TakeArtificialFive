3a31ed8a643b281932d9d755777183b81e0a6765
deWiced <anej.placer@gmail.com>
2014-08-12 02:45:27 +0200

    Gene pool and entity initialization and timer event. [#1]

--- core.cpp
+++ core.cpp
@@ -1,20 +1,96 @@
 #include "core.h"
 
+#include "random_generator.h"
+
 #include <QDebug>
+#include <iostream>
+#include <ctime>
+
+const int Core::instruments[5] = { 0, 52, 48, 35, 81 }; // 0=piano, 52=choir aahs, 48=strings, 35=fretless bass, 74=pan flute
 
 Core::Core(QObject *parent)
     : QObject(parent)
 {}
 
-void Core::initialize(int w_height, int w_width, int entities, int generations, int steps, int speed)
+void Core::initialize(int w_height, int w_width, int nr_of_entities, int nr_of_genes, int generations, int steps, int speed)
 {
     world_height = w_height;
     world_width = w_width;
 
-    number_of_entities = entities;
+    number_of_entities = nr_of_entities;
+    number_of_genes = nr_of_genes;
     number_of_generations = generations;
     steps_per_generation = steps;
     speed_ms = speed;
 
+    // SETUP EVERYTHING ...
+    initializeGenePool();
 
+    for (int i = 0; i < number_of_entities; i++) {
+
+        Entity *ent = new Entity(instruments[i % 5], i, generateInitialPosition());
+        assignGenes(number_of_genes, ent);
+        entities.append(ent);
+
+        midi_engine.setPatch(ent->getPatch(), ent->getInstrument());
+
+    }
+
+    timer_id = startTimer(speed);
+
+}
+
+void Core::processNextStep()
+{
+
+}
+
+void Core::exportWorldPositions()
+{
+    for (int i = 0; i < world_height; i++) {
+        for (int j = 0; j < world_width; j++) {
+            if (entityPresent(i, j))
+                std::cout << " O ";
+            else
+                std::cout << " - ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void Core::initializeGenePool()
+{
+    gene_pool.append(LonelyGene());
+    gene_pool.append(SeekingGene());
+}
+
+void Core::assignGenes(int amount, Entity *ent)
+{
+    for (int i = 0; i < amount; i++) {
+        ent->addGene(gene_pool[RandomGenerator::get()->random(0, gene_pool.size()-1)]);
+    }
+}
+
+QVector2D Core::generateInitialPosition()
+{
+    return QVector2D(RandomGenerator::get()->random(0, world_height-1),
+                     RandomGenerator::get()->random(0, world_width-1));
+}
+
+bool Core::entityPresent(int row, int column)
+{
+    for (int i = 0; i < entities.size(); i++) {
+        if (entities.at(i)->getPosition() == QVector2D(row, column))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void Core::timerEvent(QTimerEvent *)
+{
+    processNextStep();
+    exportWorldPositions();
 }
--- core.h
+++ core.h
@@ -1,9 +1,12 @@
 #ifndef CORE_H
 #define CORE_H
 
+#include "midi_engine.h"
 #include "entity.h"
+#include "genes/genes.h"
 
 #include <QObject>
+#include <QTimer>
 
 class Core : public QObject
 {
@@ -13,19 +16,37 @@ public:
     explicit Core(QObject *parent = 0);
 
 public:
-    void initialize(int w_height, int w_width, int entities, int generations, int steps, int speed);
+    void initialize(int w_height, int w_width, int nr_of_entities, int nr_of_genes, int generations, int steps, int speed);
 
-private:
+    void exportWorldPositions(); // vraca 2d tabelo
 
+private:    
+    void processNextStep();
+
+    void initializeGenePool();
+    void assignGenes(int amount, Entity *ent);
+    QVector2D generateInitialPosition();
+    bool entityPresent(int row, int column);
+
+    void timerEvent(QTimerEvent *);
 
 private:
     int world_height;
     int world_width;
 
     int number_of_entities;
+    int number_of_genes;
     int number_of_generations;
     int steps_per_generation;
     int speed_ms;
+
+    QVector<Gene> gene_pool;
+    QVector<Entity*> entities;
+
+    static const int instruments[5];
+
+    MidiEngine midi_engine;
+    int timer_id;
 
 };
 
