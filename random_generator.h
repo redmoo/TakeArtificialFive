e984cd45f87c10af39452393e5d03f55bc9f00e3
deWiced <anej.placer@gmail.com>
2014-08-14 01:43:10 +0200

    Reuse of custom seeds. [#6]

--- random_generator.h
+++ random_generator.h
@@ -4,6 +4,7 @@
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
 #include <ctime>
+#include <QDebug>
 
 class RandomGenerator
 {
@@ -21,9 +22,17 @@ public:
         return range(rng);
     }
 
+    time_t getSeed()
+    {
+        return seed;
+    }
+
 private:
     RandomGenerator() {
-        rng.seed(std::time(0));
+        // 1407943248 interesting seed: 4 entitete vsaka ma 2 lonely gena  1407943591 1407943337 1407943324 1407943262 (extra cool 1407946622)
+        seed = std::time(0);
+        qDebug() << "Seed: " << seed;
+        rng.seed(1407946622);
     }
 
     // Dont forget to declare these two. You want to make sure they
@@ -33,24 +42,9 @@ private:
     void operator=(RandomGenerator const&); // Don't implement
 
 private:
+    time_t seed;
     boost::random::mt19937 rng; // static?
 
-/* TEST
-public:
-    static void seed(int s)
-    {
-        rng.seed(s);
-    }
-
-    static int random(int min, int max)
-    {
-        boost::random::uniform_int_distribution<> range(min, max);
-        return range(rng);
-    }
-
-private:
-    static boost::random::mt19937 rng;
-*/
 };
 
 #endif // RANDOM_GENERATOR_H
